e9d1382e332412b27a0c56311c3302fe35b823d7
Gunnar Halen <gunnar.halen@gmail.com>
2025-09-10 23:28:05 -0300

    Add home link to login logo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,14 +2,24 @@
 
 import Logo from "@/components/Logo";
 import { LoginForm } from "@/components/LoginForm";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const Router = useRouter();
+
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <div className="flex items-center gap-2 font-medium">
-            <Logo width={120} />
+            <Button
+              variant="ghost"
+              onClick={() => Router.push("/")}
+              className="cursor-pointer hover:bg-transparent"
+            >
+              <Logo width={120} />
+            </Button>
           </div>
         </div>
         <div className="flex flex-1 items-center justify-center">
